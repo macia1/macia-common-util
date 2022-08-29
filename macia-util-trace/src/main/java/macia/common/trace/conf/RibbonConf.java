1dd6a1f361e0b8d22632e7bec7c84478d2f89b06
zenggs <zenggs_ebiz@guohualife.com>
2022-08-29 08:58:40 +0800

    补充ribbon拦截器

---
+++ macia-util-trace/src/main/java/macia/common/trace/conf/RibbonConf.java
@@ -0,0 +1,29 @@
+package macia.common.trace.conf;
+
+import macia.common.trace.resultful.ActionTrackInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import javax.annotation.Resource;
+import java.util.Collections;
+
+/**
+ * @author ebiz_zenggs
+ * @Date 2022/8/29
+ */
+@Configuration
+public class RibbonConf {
+
+    @Resource
+    ActionTrackInterceptor actionTrackInterceptor;
+
+    @Bean
+    @ConditionalOnMissingBean
+    public RestTemplate restTemplate(){
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setInterceptors(Collections.singletonList(actionTrackInterceptor));
+        return restTemplate;
+    }
+
+}
